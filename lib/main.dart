812ffa306dcf7eeb43be120a5114633bd98dd92a
Felix Dinh <felixdinhit@gmail.com>
2023-10-22 16:02:49 +0700

    app icon + splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/provider/setting_provider.dart';
 import 'package:weather_app/router/route_config.dart';
 import 'package:weather_app/router/route_name.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(const Duration(seconds: 0));
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
@@ -18,6 +22,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => SettingProvider())
       ],
       builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
         scaffoldMessengerKey: GlobalKey<ScaffoldMessengerState>(),
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
